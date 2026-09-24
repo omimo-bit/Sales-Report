2dc5134338230c889eedddea6b755dfdb660c3f9
omimo-bit <dwicky@araiagroup.com>
2026-09-24 15:45:39 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 window.KTD_CONFIG = {
   // WAJIB: ganti dengan URL /exec deployment Apps Script terbaru.
   // Contoh: https://script.google.com/macros/s/AKfy.../exec
-  GAS_WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbxbUqje4Rp7AYcVQNhiol7atL2gMXN3rFn3Svt8bEAsSFO14QKTjOgRT8IzspzghG4E/exec',
+  GAS_WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbzlFekG1zrCnJAf2DzoaIfm6mWrHdJ-T1hyss8JhZ6S5futnDI2yHg__YUy4UYaN10/exec',
 
   // Timeout request online ke Apps Script.
   REQUEST_TIMEOUT_MS: 30000
