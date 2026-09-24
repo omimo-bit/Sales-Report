ece879c1aabd7242fd6f4d3ed242e6423a1cef18
omimo-bit <dwicky@araiagroup.com>
2026-09-24 18:21:33 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.KTD_CONFIG = {
   APP_VERSION: '2026.09.24.1738-BRIDGE-V3',
-  GAS_WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbwAI6xc4CxCDEwiAgp_7NYQu_YjszdDbP9WfmZpi9HlvtXWapvZ5EOX7WyHDzxfXK3m/exec',
+  GAS_WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbyUHA-66wOCKVh_kIOWKIg633hXgh8qWLOrYRdbYnO91YVMYBFbrKM_JY3oUmh-aV18/exec',
   REQUEST_TIMEOUT_MS: 30000,
   BRIDGE_READY_TIMEOUT_MS: 12000,
   AUTO_SYNC_MS: 30000,
