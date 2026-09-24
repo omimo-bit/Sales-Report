d3bc9ec1385137268702c247e228228fe2c9ae1d
omimo-bit <dwicky@araiagroup.com>
2026-09-24 16:43:01 +0700

    Update GAS_WEB_APP_URL in config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 window.KTD_CONFIG = {
   // WAJIB: ganti dengan URL /exec deployment Apps Script terbaru.
   // Contoh: https://script.google.com/macros/s/AKfy.../exec
-  GAS_WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbzlFekG1zrCnJAf2DzoaIfm6mWrHdJ-T1hyss8JhZ6S5futnDI2yHg__YUy4UYaN10/exec',
+  GAS_WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbzuK6ZZl9jbH-l1Xoiurdrn9rsAEO8mmS-EguFkJEcCClgjvkTgc9kVQgpbWXuFYEFE/exec',
 
   // Timeout request online ke Apps Script.
   REQUEST_TIMEOUT_MS: 30000
